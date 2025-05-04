ccdfb4d9fc4fb6d95dbe2a2375af2f7291611f7d
mugur <mugurr@mail.ru>
2025-05-04 17:53:08 +0500

    Созданы: 1.	Создание модели для взаимодействия с базой данных 2.	ProductEntity.java

---
+++ src/main/java/ru/itis/servletlessontwo/model/ProductEntity.java
@@ -0,0 +1,10 @@
+package ru.itis.servletlessontwo.model;
+
+public class ProductEntity {
+    Long id;
+    String name;
+    String description;
+    double price;
+    int quantity;
+    byte[] image;
+}
