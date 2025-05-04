ed820d3d1bb656c812167ee5ff1498e26697d000
mugur <mugurr@mail.ru>
2025-05-04 17:43:20 +0500

    Созданы: 1.	Настройка подключение к БД при запуске приложения 2.	Делает это безопасно (данные берутся из конфига) 3.	Предоставляет удобный инструмент (JdbcTemplate) для всех компонентов приложения Для этого созданы MainContextListener.java PropertyReader.java app.properties

--- src/main/java/ru/itis/servletlessontwo/config/MainContextListener.java
+++ src/main/java/ru/itis/servletlessontwo/config/MainContextListener.java
@@ -1,6 +1,42 @@
 package ru.itis.servletlessontwo.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.itis.servletlessontwo.utils.PropertyReader;
 
+import javax.sql.DataSource;
+
+@Slf4j
+@WebListener
 public class MainContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        ServletContext context = sce.getServletContext(); // context - глобальное хранилище для всего веб-приложения
+
+        DataSource dataSource = dataSource();
+        context.setAttribute("dataSource", dataSource); // сохраняет объект DataSource в это хранилище под именем "dataSource"
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        context.setAttribute("jdbcTemplate", jdbcTemplate);
+    }
+
+    private DataSource dataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(PropertyReader.getProperty("DB_URL"));
+        dataSource.setUser(PropertyReader.getProperty("DB_USER"));
+        dataSource.setPassword(PropertyReader.getProperty("DB_PASSWORD"));
+        return dataSource;
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        log.info("-=-=-=-=-=-=-=-=- CONTEXT DESTROYED -==-=-=-=-=-=-=-=-=");
+    }
 }
