aab899e32d62549143dac27d54774a3b8c261767
mugur <mugurr@mail.ru>
2025-05-04 20:59:55 +0500

    добавлены аннотации lombok'а

--- src/main/java/ru/itis/servletlessontwo/model/ProductEntity.java
+++ src/main/java/ru/itis/servletlessontwo/model/ProductEntity.java
@@ -1,5 +1,12 @@
 package ru.itis.servletlessontwo.model;
 
+import lombok.*;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductEntity {
     Long id;
     String name;
