3fc1efe3a9eeeb162997ce6b81b37a449e86a900
mugur <mugurr@mail.ru>
2025-05-12 21:49:19 +0500

    создали: - интерфейс UserService

---
+++ src/main/java/ru/itis/servletlessontwo/service/UserService.java
@@ -0,0 +1,12 @@
+package ru.itis.servletlessontwo.service;
+
+import ru.itis.servletlessontwo.dto.request.SignInRequest;
+import ru.itis.servletlessontwo.dto.request.SignUpRequest;
+import ru.itis.servletlessontwo.dto.response.AuthResponse;
+import ru.itis.servletlessontwo.dto.response.UserDataResponse;
+
+public interface UserService {
+    AuthResponse signUp(SignUpRequest request);
+
+    AuthResponse signIn(SignInRequest request);
+}
