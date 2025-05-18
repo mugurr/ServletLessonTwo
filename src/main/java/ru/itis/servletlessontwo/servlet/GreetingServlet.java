e76dc2fbd989593c588d05a9d0c1a24dc4edd178
mugur <mugurr@mail.ru>
2025-05-18 19:55:51 +0500

    создали: - испраивили ошибку - в файле MainServlet.java в строке req.getRequestDispatcher("jsp/main.jsp").forward(req, resp); не было указано ".forward(req, resp)"

--- src/main/java/ru/itis/servletlessontwo/servlet/GreetingServlet.java
+++ src/main/java/ru/itis/servletlessontwo/servlet/GreetingServlet.java
@@ -8,12 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/main")
-public class MainServlet extends HttpServlet {
+@WebServlet("")
+public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        req.getRequestDispatcher("jsp/main.jsp").forward(req, resp);
+        req.getRequestDispatcher("jsp/basic.jsp").forward(req, resp);
     }
 }
